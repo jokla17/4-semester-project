9eb49dff4cdc99a3da77240041b6726b84694255
Wahid Tobias <54397547+wtrwinberg@users.noreply.github.com>
2021-04-16 10:45:07 +0200

    Update maintenance.component.ts

--- web-app/src/app/main-dashboard/maintenance/maintenance.component.ts
+++ web-app/src/app/main-dashboard/maintenance/maintenance.component.ts
@@ -8,10 +8,23 @@ import { SocketIOService } from 'src/app/socketio.service';
 })
 
 export class MaintenanceComponent {
+    public data : any;
+    public maxMaintenance : number;
+
     constructor(
         private socketIOService : SocketIOService
     ) {
-      
+        this.data = "";
+        this.maxMaintenance = 65535;
+
+        this.socketIOService.listen("data").subscribe((data) => {
+            this.data = data;
+        })
     }
 
+    public getMaintenance() : number {
+        let i : number = Number((this.data.Maintenance / this.maxMaintenance * 100).toFixed(0));
+        document.getElementById("maintenanceMeter").style.width = i + "%";
+        return i;
+    } 
 }
