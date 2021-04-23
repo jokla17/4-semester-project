c270017ede106b3db90e802a9c7a6a6f09212cf7
Wahid Tobias <54397547+wtrwinberg@users.noreply.github.com>
2021-04-23 10:32:40 +0200

    Angular Unit Tests

--- web-app/src/app/app.component.spec.ts
+++ web-app/src/app/app.component.spec.ts
@@ -1,12 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AssetsService } from './assets.service';
+import { MainBatchReports } from './main-batch-reports/main-batch-reports.component';
+import { CreateBatchComponent } from './main-dashboard/create-batch/create-batch.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { MainStatisticalAnalysis } from './main-statistical-analysis/main-statistical-analysis.component';
+import { SocketIOService } from './socketio.service';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MainHeaderComponent,
+        MainBatchReports,
+        MainDashboardComponent,
+        MainFooterComponent,
+        MainNavigationComponent,
+        MainStatisticalAnalysis,
+        CreateBatchComponent
       ],
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+      ],
+      providers: [
+        AssetsService,
+        SocketIOService
+      ]
     }).compileComponents();
   });
 
@@ -22,10 +55,49 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('web-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('web-app app is running!');
+
+  it('should create the main-batch-reports component', () => {
+    const fixture = TestBed.createComponent(MainBatchReports);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should create the main-dashboard component', () => {
+    const fixture = TestBed.createComponent(MainDashboardComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+
+  it('should create the main-footer component', () => {
+    const fixture = TestBed.createComponent(MainFooterComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+
+  it('should create the main-header component', () => {
+    const fixture = TestBed.createComponent(MainHeaderComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+
+  it('should create the main-navigation component', () => {
+    const fixture = TestBed.createComponent(MainNavigationComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+
+  it('should create the main-statistical component', () => {
+    const fixture = TestBed.createComponent(MainStatisticalAnalysis);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
+
+
+
+
+
